00dc5c2ce8925ea1cb87c480dbd5950f39b11e6f
Salsa Nadhifa <salsanadhifa00@gmail.com>
2021-10-12 22:38:47 +0700

    first commit

---
+++ BasicJava.java
@@ -0,0 +1,5 @@
+public class BasicJava {
+
+public static void main(String[] args){}
+}
+
---
+++ Biodata.java
@@ -0,0 +1,46 @@
+public class Biodata {
+    public static void main(String[] args) {
+        //biodata 
+        // String firstName = "Salsa "
+        // String lastName = "Nadhifa
+        // System.out.println(firstName + lastName);
+        // int dateOfBirth = "09"
+        // String birthMonth = "September"
+        // int yearOfBirth = "2000"
+        // System.out.println(dateOfBirth +  birthMonth + yearOfBirth )
+        // String address = "Jl.Karya Gg.Wonosobo Medan"
+        // String hoby = "baca webtoon/manga, nonton film/anime, masak"
+
+        String firstName = "First Name: Salsa ";
+        String lastName = "Last Name: Nadhifa";
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        String age = "Age: 21";
+        System.out.println(age);
+
+        String dateOfBirth = "Date of Birth: 09 September 2000";
+        System.out.println(dateOfBirth);
+
+        String address = "Address: Jl.Karya Gg.Wonosobo No.3 Medan";
+        System.out.println(address);
+
+        String phoneNumbe = "Phpne Number: 0838-6652-6600";
+        System.out.println(phoneNumbe);
+
+        String hoby = "Hoby: read webtoon/manga, watch a film/anime, cooking";
+        System.out.println(hoby);
+
+        String makananKesukaan = "Favorite food: Mie Ayam, spageti";
+        System.out.println(makananKesukaan);
+
+
+
+
+
+
+
+
+    }
+    
+}
